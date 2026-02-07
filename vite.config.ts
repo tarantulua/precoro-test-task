2762b3b6a406f31dd0a774b2b49eb583a306ecd8
Taras Pylypchyn <rever.forever1@gmail.com>
2026-02-07 12:16:09 +0200

    fix: Fix tooltip on mobile devices

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath } from "node:url";
 
+console.log("test", process.env.VITE_BASE_PATH);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -10,5 +12,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: process.env.VITE_BASE_PATH || "precoro-test-task",
+  base: process.env.VITE_BASE_PATH || "/",
 });
